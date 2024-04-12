e0726b04c560096bb23747d09072802d56ab13fb
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-12 21:51:15 +0500

    my first commit

---
+++ Q.4 Famous-Quote/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let quote = "A person who never made a mistake never tried anything new";
+let author = "Albert Einstien";
+console.log(`${author} once said,"${quote}"`);
---
+++ Q.4 Famous-Quote/main.ts
@@ -0,0 +1,7 @@
+let quote: string =
+  "A person who never made a mistake never tried anything new";
+
+  let author: string = "Albert Einstien";
+   
+  console.log(`${author} once said,"${quote}"`);
+  

3ebd131c9c0cebe233822519dfcb198f0fbc518b
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-14 20:14:49 +0500

    my first commit

---
+++ Q.14 Guest-List/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let guestList = ["Fatima", "Iqra", "Mehak"];
+for (let i = 0; i < guestList.length; i++) {
+    console.log("Dear Mrs. " + guestList[i] + " , \n\nit is our pleasure to invite you in our party.\n\nThank you!\n\n");
+}
---
+++ Q.14 Guest-List/main.ts
@@ -0,0 +1,6 @@
+let guestList: string[] = ["Fatima", "Iqra", "Mehak"];
+
+for (let i=0; i<guestList.length; i++) {
+  console.log( "Dear Mrs. " + guestList[i] + " , \n\nit is our pleasure to invite you in our party.\n\nThank you!\n\n"
+  );
+}

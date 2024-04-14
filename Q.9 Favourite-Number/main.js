e5c988ab797f9a7463e8ee8676250c9bfc56ac15
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-14 12:04:07 +0500

    my first commit

---
+++ Q.9 Favourite-Number/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const myFavouriteNumber = 7;
+let message = `My Favourite Number is : ${myFavouriteNumber}`;
+console.log(message);
---
+++ Q.9 Favourite-Number/main.ts
@@ -0,0 +1,6 @@
+
+const myFavouriteNumber: number = 7;
+
+let message : string = `My Favourite Number is : ${myFavouriteNumber}`;
+
+console.log(message);

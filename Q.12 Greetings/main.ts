fa72eedd84ead5bae723764ad89a41cd6b61477b
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-14 12:52:24 +0500

    my first commit

---
+++ Q.12 Greetings/main.ts
@@ -0,0 +1,11 @@
+// Array of friends name
+
+let friendsName: string[] = ["Fatima", "Iqra", "Mehak"];
+
+let message: string = "would you like to read typescript with me?";
+
+// using for loop for accessing each element and printing message to every friend
+
+for(let i = 0; i<friendsName.length; i++){
+    console.log(message + "" ,friendsName[i]);
+} 

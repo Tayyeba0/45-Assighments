85f739d72aa59b88c776bcb70a4b029e6935f2c1
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-14 12:33:11 +0500

    my first commit

---
+++ Q.11 Names/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Array of friends
+let friendsNames = ["Fatima", "Iqra", "Mehak"];
+// using for loop for accessing each element of array one by one
+for (let i = 0; i < friendsNames.length; i++) {
+    console.log(friendsNames[i]);
+}
---
+++ Q.11 Names/main.ts
@@ -0,0 +1,10 @@
+
+// Array of friends
+
+let friendsNames: string[] = ["Fatima","Iqra","Mehak"];
+
+// using for loop for accessing each element of array one by one
+
+for(let i = 0; i<friendsNames.length; i++){
+    console.log(friendsNames[i]);
+}

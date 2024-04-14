e742b860aff42d26cbc49bfd7ab862222dc6bc9d
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-14 13:03:27 +0500

    my first commit

---
+++ Q.13 Your-Own-Array/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// array of transportation
+let transportation = ["car", "motorbike", "cycle"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log("i would like to own a " + '', transportation[i]);
+}
---
+++ Q.13 Your-Own-Array/main.ts
@@ -0,0 +1,8 @@
+// array of transportation
+let transportation: string[]= ["car","motorbike","cycle"];
+
+
+
+for(let i=0; i<transportation.length; i++){
+    console.log("i would like to own a " + '', transportation[i]);
+}

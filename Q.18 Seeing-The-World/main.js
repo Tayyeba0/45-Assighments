90791f28fd2d64e0f72a24fb07b0f610d3f2425c
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-15 18:29:58 +0500

    my first commit

---
+++ Q.18 Seeing-The-World/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// array of world places
+let places = ['Italy', 'Germany', 'Agra', 'EifelTower', 'Time Square'];
+console.log('Origional:' + places);
+console.log('Copy:' + [...places].sort());
+console.log('Origional:' + places);
+console.log('Copy:' + [...places].sort().reverse());
+console.log('Origional:' + places);
+console.log('Origional:' + places.reverse());
+console.log('Origional:' + places.sort());
+console.log('Origional:' + places.sort().reverse());
---
+++ Q.18 Seeing-The-World/main.ts
@@ -0,0 +1,19 @@
+// array of world places
+
+let places: string[] = ['Italy','Germany', 'Agra','EifelTower','Time Square'];
+
+console.log('Origional:' + places);
+
+console.log('Copy:' + [...places].sort());
+
+console.log('Origional:'+ places);
+
+console.log('Copy:' + [...places].sort().reverse());
+
+console.log('Origional:' + places);
+
+console.log('Origional:' + places.reverse());
+
+console.log('Origional:' + places.sort());
+
+console.log('Origional:' + places.sort().reverse());

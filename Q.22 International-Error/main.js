2642b0066eddd5b594d4ea038e544b6ea48872c9
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-16 12:04:11 +0500

    my first commit

---
+++ Q.22 International-Error/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let newArray = ['river', 'mountain', 'sea'];
+// accsseing array by index
+console.log(newArray[5]);
+// correct the error
+console.log(newArray);
---
+++ Q.22 International-Error/main.ts
@@ -0,0 +1,9 @@
+
+let newArray : string[] = ['river','mountain','sea'];
+
+// accsseing array by index
+
+console.log(newArray[5]);
+
+// correct the error
+console.log(newArray);

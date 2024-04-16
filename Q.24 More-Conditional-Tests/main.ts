fc090149d5a14163a64176dde4e82ec551d1565e
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-16 13:23:34 +0500

    my first commit

---
+++ Q.24 More-Conditional-Tests/main.ts
@@ -0,0 +1,33 @@
+// Equality and Inequality Test 1
+console.log("Equality test with strings:", "Apple" === "Apple");
+
+// Equality and inequality test 2
+console.log("inequality test with strings:", ("Apple" as string) != "orange");
+
+// test usingthe lower case function
+console.log("Lowercase function test:","HELLO".toLowerCase() === "hello");
+
+// Numerical tests involving equality and inequality
+console.log("Equality test with numbers:",26 === 26);
+console.log("inequality test with numbers:", (26 as number) != 35);
+
+// greater than and less than
+console.log("Greater than test:", 10 > 5);
+console.log("Less than test:", 10 < 5);
+// greater than and equal to 
+console.log("Greater than and Equal to test:", 10 >= 10);
+
+// less than or equal to
+console.log("Less than or equal to test:", 5 <= 10);
+
+// tests using "and" and "or" operator
+console.log("And operator test:", 5===5 && 10 > 5);
+console.log("OR operator test:", 5===5 || false);
+
+// Test whether an item is not in a array
+
+const fruits : string[] = ["Pineapple","Banana","Apple"];
+console.log('Test "Pineapple" in the array:', fruits.includes("pineapple"));
+
+// test whether an item is not in an array
+console.log('Testing "strawberry" is not in array:', !fruits.includes("strawberry"));

9c154a8738ad266233a06d08bce32c6ed0782900
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-17 14:53:25 +0500

    my first commit

---
+++ Q.29 Favourite _Fruit/main.ts
@@ -0,0 +1,19 @@
+// array of favourite fruit
+
+let favouriteFruit: string[] = ['apple','banana','cherry'];
+
+if(favouriteFruit.includes('apple')){
+    console.log('I really like apple');
+}
+if(favouriteFruit.includes('banana')){
+    console.log('I really like banana.');
+}
+if(favouriteFruit.includes('cherry')){
+    console.log('I really like cherry.');
+}
+if(favouriteFruit.includes('mango')){
+    console.log('I really like mango.');
+}
+if(favouriteFruit.includes('orange')){
+    console.log('I really like orange.');
+}

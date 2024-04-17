ffcf09fbbd8950040dd760c5ab58d019885cfdc0
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-17 10:24:47 +0500

    my first commit

---
+++ Q.26 Alien_Color#2/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Alien Color 2
+// choose a color for an alien as you did in exercise 25
+let alienColor = 'green';
+if (alienColor === 'green') {
+    console.log('player earned 5 points for shooting the alien.');
+}
+else {
+    console.log('player earned 10 points.');
+}
+// if the alien color is not green print a statement that the player just earned 10 points.
+alienColor = 'yellow';
+if (alienColor === 'green') {
+    console.log('player earned 5 points for shooting the alien.');
+}
+else {
+    console.log('player just earned 10 points.');
+}

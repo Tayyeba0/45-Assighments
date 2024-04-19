89c39a3e9b07fa606ebd4609c4aa65b276d0ea97
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-19 13:38:35 +0500

    my first commit

---
+++ Q.35 Animals/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let Animals = ['dog', 'cat', 'rabbit'];
+for (let Animal of Animals) {
+    console.log(`A ${Animal} would make a great pet.`);
+}
+console.log(`Any of these animals would make a great pet!`);
---
+++ Q.35 Animals/main.ts
@@ -0,0 +1,7 @@
+let Animals : string[] = ['dog','cat','rabbit'];
+
+for(let Animal of Animals){
+    console.log(`A ${Animal} would make a great pet.`);
+}
+
+console.log(`Any of these animals would make a great pet!`);

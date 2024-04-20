839130b81b2507ab2820e4943143844bb8b68a72
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-20 10:23:55 +0500

    my first commit

---
+++ Q.36 T_Shirt/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// making function
+function makeShirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+makeShirt('large', 'Hi Twetty');
---
+++ Q.36 T_Shirt/main.ts
@@ -0,0 +1,5 @@
+// making function
+function makeShirt(size: string , text: string){
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+makeShirt('large','Hi Twetty');

3884d0a13e7b22744d05079f763cb382779c9e41
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-23 10:16:42 +0500

    my first commit

---
+++ Q.41 Magicians/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ['Harry Potter', 'Hermione Granger', 'Ron Wesley'];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magicians);
---
+++ Q.41 Magicians/main.ts
@@ -0,0 +1,11 @@
+
+let magicians: string[] = ['Harry Potter', 'Hermione Granger', 'Ron Wesley'];
+
+function show_magicians(magicians:string[]){
+
+    magicians.forEach(element => {
+        console.log(element);
+        
+    });
+}
+show_magicians(magicians);

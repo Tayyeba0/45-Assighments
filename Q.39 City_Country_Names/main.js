d0c307c7699f6a29110b9051a14233834cfaa338
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-23 09:52:36 +0500

    my first commit

---
+++ Q.39 City_Country_Names/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    return `${city} , ${country}`;
+}
+console.log(city_country("Japan", "Tokyo"));
+console.log(city_country("France", "Paris"));
+console.log(city_country("Multan", "Pakistan"));
---
+++ Q.39 City_Country_Names/main.ts
@@ -0,0 +1,12 @@
+
+function city_country(city: string , country: string){
+    return `${city} , ${country}`;
+}
+
+console.log(city_country("Japan","Tokyo"));
+
+console.log(city_country("France", "Paris"));
+
+console.log(city_country("Multan", "Pakistan"));
+
+

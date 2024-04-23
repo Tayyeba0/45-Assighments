127a27dc364cce5d11545bf4a2e5e836bb4332ad
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-23 09:35:26 +0500

    my first commit

---
+++ Q.38 Cities/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function describe_city(city, country = 'Pakistan') {
+    console.log(`${city} is in ${country}`);
+}
+describe_city('Riaz', 'Saudia_Arabia');
+describe_city('France', 'Paris');
+describe_city('Karachi');
---
+++ Q.38 Cities/main.ts
@@ -0,0 +1,10 @@
+
+function describe_city(city:string, country:string = 'Pakistan'){
+    console.log(`${city} is in ${country}`);
+}
+
+describe_city('Riaz','Saudia_Arabia');
+
+describe_city('France','Paris');
+
+describe_city('Karachi');
